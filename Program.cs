6595eb556bd808c6a6127c44532953c9fd6f373f
Eleonora <ekadyrova107@gmail.com>
2022-11-07 01:01:40 +0500

    task 55 has been debugged

--- Program.cs
+++ Program.cs
@@ -40,6 +40,31 @@ bool PozArrayTwoDim(int row, int col, double[,] array)
     return false;
 }
 
+double[] ArithMeanCol(int[,] array)
+{
+    int numStr = array.GetLength(0);
+    int numRow = array.GetLength(1);
+    double[] arrayMean = new double[numRow];   //создание массива для средних арифметических
+    
+    double tmp = 0;
+
+    for (int r = 0; r < numStr; r++) {  // вывод массива на экран для наглядности
+        for (int c = 0; c < numRow; c++) Console.Write($"{array[r, c]}\t");
+        Console.WriteLine();
+    }
+
+    
+    for (int i = 0; i < numRow; i++)  {   //вычисление суммы чисел столбца
+        for (int j = 0; j < numStr; j++) {
+            tmp = tmp + array[j, i];
+        }
+
+        arrayMean[i] = (double)(tmp / numStr);  // среднее арифметическое столбца
+        tmp = 0;
+    }
+return arrayMean;
+}
+
 Console.WriteLine("Урок 7 - работа с двумерным массивом\n");
 Console.Write("Задача 47. Создание двумерного массива\n");
     
@@ -62,7 +87,6 @@ if (taskCharPrint == "y") {       // вывод массива на печать
 }   
 
 Console.Write("Задача 50. Есть ли в массиве позиция с такими индексами\n");
-Console.Write("Максимальная величина элемента массива 9 для красоты вывода\n");
     
 Console.Write("Задайте номер строки\t> ");
 int tmpRow = Convert.ToInt32(Console.ReadLine());
@@ -74,6 +98,24 @@ bool poz = PozArrayTwoDim(tmpRow, tmpCol, tmpArray);
 
 if (poz == true) Console.WriteLine("Элемент с такими индексами в массиве есть");
 else Console.WriteLine("Элемента с такими индексами в массиве нет");
+
+
+Console.Write("\n\nЗадача 55. Среднее арифметическое столбцов массива\n");
+Console.Write("Максимальная величина элемента массива 9 для красоты вывода\n");
+    
+Console.Write("Задайте количество строк\t> ");
+int t55Row = Convert.ToInt32(Console.ReadLine());
+    
+Console.Write("Задайте количество столбцов\t> ");
+int t55Col = Convert.ToInt32(Console.ReadLine());
+
+int[,] t55Array = CreateArrayIntTwoDim(t55Row, t55Col); //создание двумерного массива (элементы <10)
+
+double[] arithMean = ArithMeanCol(t55Array); // вычисление массива средних арифметических
+
+for (int r = 0; r < arithMean.Length; r++) Console.Write($"{Math.Round(arithMean[r], 1)}\t");
+Console.WriteLine();
+
   
 
 
